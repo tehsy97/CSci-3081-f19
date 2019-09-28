ee1a75925f58f6bc3711f7c6ec98ab9e27b81f33
Shin Yin Teh <teh00001@umn.edu>
2019-09-28 14:32:16 -0500

    jan to march

--- labs/lab06_google_tests/date_unittest.cc
+++ labs/lab06_google_tests/date_unittest.cc
@@ -18,7 +18,6 @@ class DateTest : public ::testing::Test {
   ind_day = Date(1776, 7, 4);            // US Independence
   best_holiday = Date(2018, 10, 31);     // Halloween
   tomorrow = date_today + 1;    
-  answer_day = ind_day - 1;
   epoch = Date(0);
   class_start_epoch = Date(1536066000);   // 2018-09-04 13:00:00
   }
@@ -33,6 +32,7 @@ class DateTest : public ::testing::Test {
   Date date_today;
   Date epoch;
   Date class_start_epoch;  
+  int answer_num_days;
 };
 
 
@@ -116,6 +116,7 @@ TEST(DateTest, ConvertFromDays) {
 */
 
 TEST_F(DateTest, GetDateTests) {
+  answer_day = ind_day - 1;
 
   std::time_t t = std::time(NULL);
   std::tm * now = std::localtime(&t);
@@ -160,7 +161,6 @@ TEST_F(DateTest, LeadingZeroTests) {
   std::string expected_out_5 = "10-31-2018\n";
   std::string expected_out_6 = "01-01-1970\n";
   std::string expected_out_7 = "09-04-2018\n";
-
   
   testing::internal::CaptureStdout();
   date_today.PrintUsDate(true);
@@ -205,12 +205,74 @@ TEST_F(DateTest, LeadingZeroTests) {
   EXPECT_EQ(output8, std::string(s));
 }
 
+
+TEST_F(DateTest, PrintUsDateWithoutNewLineTestsfalse) {
+  std::time_t t = std::time(NULL);
+  std::tm * now = std::localtime(&t);
+  char s[100];
+
+  sprintf(s, "%02d-%02d-%d", now->tm_mon+1, now->tm_mday, now->tm_year+1900);
+  std::string expected_out_1 = std::string(s); 
+  std::string expected_out_2 = "12-31-1999";
+  std::string expected_out_3 = "09-04-2018";
+  std::string expected_out_4 = "07-04-1776";
+  std::string expected_out_5 = "10-31-2018";
+  std::string expected_out_6 = "01-01-1970";
+  std::string expected_out_7 = "09-04-2018";
+  
+  testing::internal::CaptureStdout();
+  date_today.PrintUsDate(false);
+  std::string output1 = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout();
+  y2k.PrintUsDate(false);
+  std::string output2 = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout();
+  first_day.PrintUsDate(false);
+  std::string output3 = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout(); // You must do this EVERY TIME, or else it will segfault
+  ind_day.PrintUsDate(false);
+  std::string output4 = testing::internal::GetCapturedStdout();
+  
+  testing::internal::CaptureStdout();
+  best_holiday.PrintUsDate(false);
+  std::string output5 = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout();
+  epoch.PrintUsDate(false);
+  std::string output6 = testing::internal::GetCapturedStdout();
+  
+  testing::internal::CaptureStdout();
+  class_start_epoch.PrintUsDate(false);
+  std::string output7 = testing::internal::GetCapturedStdout();
+
+  testing::internal::CaptureStdout();
+  tomorrow.PrintUsDate(false);
+  std::string output8 = testing::internal::GetCapturedStdout();
+  
+  EXPECT_EQ(output1, expected_out_1); // date_today
+  EXPECT_EQ(output2, expected_out_2);
+  EXPECT_EQ(output3, expected_out_3);
+  EXPECT_EQ(output4, expected_out_4);
+  EXPECT_EQ(output5, expected_out_5);
+  EXPECT_EQ(output6, expected_out_6);
+  EXPECT_EQ(output7, expected_out_7);
+  sprintf(s, "%02d-%02d-%d", now->tm_mon+1, now->tm_mday+1, now->tm_year+1900);
+  EXPECT_EQ(output8, std::string(s));
+}
+
+
+
 TEST_F(DateTest, TodayDateTests) {
   time_t t = std::time(NULL);
   tm * now = std::localtime(&t);
   char s[100];
+
   sprintf(s, "%d-%02d-%02d", now->tm_year+1900, now->tm_mon+1, now->tm_mday);
   EXPECT_EQ(date_today.GetDate(), std::string(s)) << "Today's date not setup properly";
+
   testing::internal::CaptureStdout();
   date_today.PrintDate(false);
   std::string output = testing::internal::GetCapturedStdout();
@@ -271,63 +333,51 @@ TEST_F(DateTest, GetUsDateTests) {
   EXPECT_EQ(output8, std::string(s));
 }
 
-// TEST_F(DateTest, operatorPlusTests) {
-//   Date date_today;
-//   Date y2k(1999, 12, 31);              // y2k
-//   Date first_day(2018, 9, 4);          // first day of classes
-//   Date ind_day(1776, 7, 4);            // US Independence
-//   Date best_holiday(2018, 10, 31);     // Halloween
-//   Date epoch(0);
-//   Date class_start_epoch(1536066000);  // 2018-09-04 13:00:00
-//   Date tomorrow = date_today + 1; 
+TEST_F(DateTest, SomeOperationTests) {
+  char s[100];
+  answer_num_days = first_day.DaysBetween(last_day);
+  sprintf(s, "%d", answer_num_days);
+  EXPECT_EQ(std::string(s), "98") << "answer_number_days not setup properly";
 
-//   std::string expected_out_1 = "09-26-2019\n";
-//   std::string expected_out_2 = "12-31-1999\n";
-//   std::string expected_out_3 = "09-04-2018\n";
-//   std::string expected_out_4 = "07-04-1776\n";
-//   std::string expected_out_5 = "10-31-2018\n";
-//   std::string expected_out_6 = "01-01-1970\n";
-//   std::string expected_out_7 = "09-04-2018\n";
-//   std::string expected_out_8 = "09-27-2019\n";
+  answer_day = first_day + answer_num_days;
+
+  std::string expected_out_1 = "2018-12-11\n";
+  std::string expected_out_2 = "2018-12-11";
+
+  testing::internal::CaptureStdout();
+  answer_day.PrintDate(true);
+  std::string output1 = testing::internal::GetCapturedStdout();
   
-//   testing::internal::CaptureStdout();
-//   date_today.PrintUsDate(true);
-//   std::string output1 = testing::internal::GetCapturedStdout();
+  testing::internal::CaptureStdout();
+  answer_day.PrintDate(false);
+  std::string output2 = testing::internal::GetCapturedStdout();
 
-//   testing::internal::CaptureStdout();
-//   y2k.PrintUsDate(true);
-//   std::string output2 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output1, expected_out_1);
+  EXPECT_EQ(output2, expected_out_2);
 
-//   testing::internal::CaptureStdout();
-//   first_day.PrintUsDate(true);
-//   std::string output3 = testing::internal::GetCapturedStdout();
+  answer_day = y2k + 1;
 
-//   testing::internal::CaptureStdout(); // You must do this EVERY TIME, or else it will segfault
-//   ind_day.PrintUsDate(true);
-//   std::string output4 = testing::internal::GetCapturedStdout();
+  std::string expected_out_3 = "2000-01-01\n";
+  std::string expected_out_4 = "2000-01-01";
+
+  testing::internal::CaptureStdout();
+  answer_day.PrintDate(true);
+  std::string output3 = testing::internal::GetCapturedStdout();
   
-//   testing::internal::CaptureStdout();
-//   best_holiday.PrintUsDate(true);
-//   std::string output5 = testing::internal::GetCapturedStdout();
+  testing::internal::CaptureStdout();
+  answer_day.PrintDate(false);
+  std::string output4 = testing::internal::GetCapturedStdout();
 
-//   testing::internal::CaptureStdout();
-//   epoch.PrintUsDate(true);
-//   std::string output6 = testing::internal::GetCapturedStdout();
-  
-//   testing::internal::CaptureStdout();
-//   class_start_epoch.PrintUsDate(true);
-//   std::string output7 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output3, expected_out_3);
+  EXPECT_EQ(output4, expected_out_4);
 
-//   testing::internal::CaptureStdout();
-//   tomorrow.PrintUsDate(true);
-//   std::string output8 = testing::internal::GetCapturedStdout();
+  Date jan_F (2016,01,01);
+  Date march_F (2016,03,01);
+  answer_num_days = march_F.DaysBetween(jan_F);
+  sprintf(s, "%d", answer_num_days); 
+  EXPECT_EQ(std::string(s), "60") << "DaysBetween not set up properly";
   
-//   EXPECT_EQ(output1, expected_out_1);
-//   EXPECT_EQ(output2, expected_out_2);
-//   EXPECT_EQ(output3, expected_out_3);
-//   EXPECT_EQ(output4, expected_out_4);
-//   EXPECT_EQ(output5, expected_out_5);
-//   EXPECT_EQ(output6, expected_out_6);
-//   EXPECT_EQ(output7, expected_out_7);
-//   EXPECT_EQ(output8, expected_out_8);
-// }
+}
+
+
+

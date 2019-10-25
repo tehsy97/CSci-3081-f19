120008d5675388c8f88b139b1f53e3b2735c7460
Shin Yin Teh <teh00001@umn.edu>
2019-10-25 11:28:32 -0500

    add external file

---
+++ external/gtest/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/teh00001/3081_f19/repo-teh00001/external/gtest/src
+# Build directory: /home/teh00001/3081_f19/repo-teh00001/external/gtest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ external/gtest/build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/teh00001/3081_f19/repo-teh00001/external/gtest/src/googletest
+# Build directory: /home/teh00001/3081_f19/repo-teh00001/external/gtest/build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

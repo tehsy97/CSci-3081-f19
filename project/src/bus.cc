2b19daa5483c23aeea663bb3bfdba59932551822
Shin Yin Teh <teh00001@umn.edu>
2019-11-07 23:24:33 -0600

    fix style

--- project/src/bus.cc
+++ project/src/bus.cc
@@ -42,7 +42,7 @@ int Bus::UnloadPassenger(int stop_id) {
 
 bool Bus::Move() {
   distance_remaining_ = distance_remaining_ - 1;
-  if(distance_remaining_ < 0){
+  if (distance_remaining_ < 0) {
     distance_remaining_ = 0;
   }
   return true;
